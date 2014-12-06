2ee3a17caca8d67add5bf9e1ea07e0932098b270
JJ Idt <johnidt@gmail.com>
2014-12-06 14:15:30 -0800

    trim whitespace on header line labels before validation

--- modules/album-parse.js
+++ modules/album-parse.js
@@ -11,7 +11,7 @@ module.exports = function albumParse (csvFile, callback) {
 	headerLine = csvLines.shift().split(',');
 
 	headerLine = _.map(headerLine, function(label) {
-		return label.toLowerCase();
+		return label.toLowerCase().trim();
 	});
 
 	if (!_.isEqual(headerLine, ['artist', 'album', 'release year', 'rating'])) {
