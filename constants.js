77f028e0e66b8691e131be3046997c77024c967d
JJ Idt <johnidt@gmail.com>
2014-12-09 10:15:30 -0800

    remove users route

--- constants.js
+++ constants.js
@@ -1,6 +1,6 @@
 module.exports = {
 	success: 'item(s) were successfully added to the album list',
-	fail: 'item(s) were not added, please make sure all records in your csv file have values for both Artist and Album',
+	fail: 'item(s) were not added, these are either duplicates or have missing Artist or Album fields',
 	noFile: 'no file selected, please choose a file to upload and try again',
 	wrongType: 'wrong file type, please upload a csv file'
 };
--- routes/index.js
+++ routes/index.js
@@ -31,7 +31,7 @@ router.get('/albums', function (req, res) {
 /* POST album information */
 router.post('/album', multer(multSettings), function (req, res) {
 	if(req.files && req.files.album_csv && req.files.album_csv.extension === 'csv') {
-		console.log(req.files.album_csv);
+		
 		albumParse(req.files.album_csv.buffer, function (err, data) {
 
 			if (err) {
@@ -39,7 +39,7 @@ router.post('/album', multer(multSettings), function (req, res) {
 				res.redirect('back');
 			}
 
-			saveItems(data, function(failedItems) {
+			saveItems(data, function (failedItems) {
 				req.flash('success', (data.length - failedItems.length) + ' ' + constants.success);
 				if (failedItems.length > 0) {
 					req.flash('fail', failedItems.length + ' ' + constants.fail);
@@ -66,7 +66,7 @@ function saveItems(data, cb) {
 	var failedItems = [];
 
 	async.each(data, function (item, callback) {
-		new Record(item).save(function(error) {
+		new Record(item).save(function (error) {
 			if (error) { 
 				failedItems.push(item);
 			}
